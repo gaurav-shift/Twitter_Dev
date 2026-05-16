320368eeeae6b6bd2b85e3624263071d996bfedc
gaurav-shift <agarwalgaurav302005@gmail.com>
2026-05-16 23:54:57 +0530

    Added toggle like api

---
+++ src/controllers/like-controller.js
@@ -0,0 +1,22 @@
+import LikeService from '../services/like-service.js';
+
+const likeService = new LikeService();
+export const toggleLike = async (req,res) => {
+    try {
+        const response = await likeService.toggleLike(req.query.modelId, req.query.modelType,req.body.userId);
+        res.status(200).json({
+            success: true,
+            data: response,
+            message: 'Successfully toggled like',
+            err: {}
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            data: {},
+            message: 'Something went wrong',
+            err: error
+        })
+    }
+}
